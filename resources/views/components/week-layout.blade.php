3c43d997970c1ff24077720194dbf39e368a58a8
wedyzza <ggwp.ezpezlmon@mail.ru>
2024-04-18 11:43:54 +0500

    style fixups

--- resources/views/components/week-layout.blade.php
+++ resources/views/components/week-layout.blade.php
@@ -1,10 +1,10 @@
 @props(['kid', 'week', 'weekOrders'])
 
 <div id="day-choice">
-    <a href="/kids/{{ $kid->id }}/order?date={{ $week[0]->format('Y-m-d') }}" class="day {{ $weekOrders[0] ? 'menu-selected' : '' }}">ПН</a>
-    <a href="/kids/{{ $kid->id }}/order?date={{ $week[1]->format('Y-m-d') }}" class="day {{ $weekOrders[1] ? 'menu-selected' : '' }}">ВТ</a>
-    <a href="/kids/{{ $kid->id }}/order?date={{ $week[2]->format('Y-m-d') }}" class="day {{ $weekOrders[2] ? 'menu-selected' : '' }}">СР</a>
-    <a href="/kids/{{ $kid->id }}/order?date={{ $week[3]->format('Y-m-d') }}" class="day {{ $weekOrders[3] ? 'menu-selected' : '' }}">ЧТ</a>
-    <a href="/kids/{{ $kid->id }}/order?date={{ $week[4]->format('Y-m-d') }}" class="day {{ $weekOrders[4] ? 'menu-selected' : '' }}">ПТ</a>
-    <a href="/kids/{{ $kid->id }}/order?date={{ $week[5]->format('Y-m-d') }}" class="day {{ $weekOrders[5] ? 'menu-selected' : '' }}">СБ</a>
+    <a href="/kids/{{ $kid->id }}/order?date={{ $week[0]->format('Y-m-d') }}" class="day {{ $weekOrders[0] ? 'menu-selected' : '' }} {{ $week[0]->format('Y-m-d') == request('date') ? 'active-day' : '' }}">ПН</a>
+    <a href="/kids/{{ $kid->id }}/order?date={{ $week[1]->format('Y-m-d') }}" class="day {{ $weekOrders[1] ? 'menu-selected' : '' }} {{ $week[1]->format('Y-m-d') == request('date') ? 'active-day' : '' }}">ВТ</a>
+    <a href="/kids/{{ $kid->id }}/order?date={{ $week[2]->format('Y-m-d') }}" class="day {{ $weekOrders[2] ? 'menu-selected' : '' }} {{ $week[2]->format('Y-m-d') == request('date') ? 'active-day' : '' }}">СР</a>
+    <a href="/kids/{{ $kid->id }}/order?date={{ $week[3]->format('Y-m-d') }}" class="day {{ $weekOrders[3] ? 'menu-selected' : '' }} {{ $week[3]->format('Y-m-d') == request('date') ? 'active-day' : '' }}">ЧТ</a>
+    <a href="/kids/{{ $kid->id }}/order?date={{ $week[4]->format('Y-m-d') }}" class="day {{ $weekOrders[4] ? 'menu-selected' : '' }} {{ $week[4]->format('Y-m-d') == request('date') ? 'active-day' : '' }}">ПТ</a>
+    <a href="/kids/{{ $kid->id }}/order?date={{ $week[5]->format('Y-m-d') }}" class="day {{ $weekOrders[5] ? 'menu-selected' : '' }} {{ $week[5]->format('Y-m-d') == request('date') ? 'active-day' : '' }}">СБ</a>
 </div>
